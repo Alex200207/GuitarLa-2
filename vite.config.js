d9b06eeba2c092d91e267a28ca2b2e8352cadda2
soy eddy <soyeddy.email.com>
2024-09-29 01:19:53 -0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import process from 'node:process';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import process from "node:process";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Asegúrate de que los archivos se generen en 'dist'
+  },
   server: {
-    host: '0.0.0.0', // Asegúrate de que tu aplicación escuche en todas las interfaces de red
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173, // Usar el puerto asignado por Render o el predeterminado
+    host: "0.0.0.0", // Escuchar en todas las interfaces de red
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
   },
 });
